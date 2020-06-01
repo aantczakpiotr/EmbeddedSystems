4c37f5ce72ea121a6e5fe7b9cf67ad9cdb76ee77
aantczakpiotr <224248@edu.p.lodz.pl>
2020-06-01 12:07:34 +0200

    added-basic-Accelerometer-controls-and-Light-sensor

--- BalanceApp/app/src/main/java/com/example/balanceapp/GameView.java
+++ BalanceApp/app/src/main/java/com/example/balanceapp/GameView.java
@@ -9,6 +9,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
 import android.hardware.SensorManager;
 import android.text.TextPaint;
 import android.util.AttributeSet;
@@ -18,186 +19,54 @@ import android.view.View;
  * TODO: document your custom view class.
  */
 public class GameView extends View {
-    //    private String mExampleString; // TODO: use a default from R.string...
-//    private int mExampleColor = Color.RED; // TODO: use a default from R.color...
-//    private float mExampleDimension = 0; // TODO: use a default from R.dimen...
-//    private Drawable mExampleDrawable;
-//
-//    private TextPaint mTextPaint;
-//    private float mTextWidth;
-//    private float mTextHeight;
-    //OUR RESOURCES
-    private SensorManager sensorManager;
-    private Sensor sensorAccelerometer;
-    private Bitmap bitmapB;
-    private Bitmap bitmapBackground;
-    private Bitmap bitmapH;
+
+    private static final int BALL_RADIUS = 100;
+    private Paint ballColor;
+    private Paint holeColor;
+    private Color bgColor;
+    private int viewWidth;
+    private int viewHeight;
+    private int posX;
+    private int posY;
+
 
     public GameView(Context context) {
         super(context);
-        Bitmap bitmapBall = BitmapFactory.decodeResource(getResources(), R.drawable.ball);
-        bitmapB = Bitmap.createScaledBitmap(bitmapBall, 32, 32, true);
+        ballColor = new Paint();
+        ballColor.setColor(Color.rgb(0, 153, 255));
+        holeColor = new Paint();
+        holeColor.setColor(Color.rgb(30, 30, 30));
 
-        Bitmap bitmapHole = BitmapFactory.decodeResource(getResources(), R.drawable.hole);
-        bitmapH = Bitmap.createScaledBitmap(bitmapHole, 300, 300, true);
 
-        bitmapBackground = BitmapFactory.decodeResource(getResources(), R.drawable.bg);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        canvas.drawBitmap(bitmapBackground,0,0,null);
-        canvas .drawBitmap(bitmapH, 400, 700,null);
+        canvas.drawColor(Color.rgb(56,56,56));
+        canvas.drawCircle(viewWidth/2, viewHeight/2, BALL_RADIUS*2, holeColor);
+        canvas.drawCircle(posX, posY, BALL_RADIUS, ballColor);
+        invalidate();
+    }
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        viewWidth = w;
+        viewHeight = h;
+    }
+
+    public void accelerometerEvent(SensorEvent event) {
+        posX = posX - (int) event.values[0] * 8;
+        posY = posY + (int) event.values[1] * 8;
+    }
+
+    public void lightSensorEvent(SensorEvent event) {
+        if ((int)event.values[0] < 20){
+            holeColor.setColor(Color.rgb(255,255,255));
+            ballColor.setColor(Color.rgb(204, 22, 74));
+        }else {
+            ballColor.setColor(Color.rgb(0, 153, 255));
+            holeColor.setColor(Color.rgb(30, 30, 30));
+        }
     }
 }
 
-//    private void init(AttributeSet attrs, int defStyle) {
-//        // Load attributes
-//        final TypedArray a = getContext().obtainStyledAttributes(
-//                attrs, R.styleable.GameView, defStyle, 0);
-//
-//        mExampleString = a.getString(
-//                R.styleable.GameView_exampleString);
-//        mExampleColor = a.getColor(
-//                R.styleable.GameView_exampleColor,
-//                mExampleColor);
-//        // Use getDimensionPixelSize or getDimensionPixelOffset when dealing with
-//        // values that should fall on pixel boundaries.
-//        mExampleDimension = a.getDimension(
-//                R.styleable.GameView_exampleDimension,
-//                mExampleDimension);
-//
-//        if (a.hasValue(R.styleable.GameView_exampleDrawable)) {
-//            mExampleDrawable = a.getDrawable(
-//                    R.styleable.GameView_exampleDrawable);
-//            mExampleDrawable.setCallback(this);
-//        }
-//
-//        a.recycle();
-//
-//        // Set up a default TextPaint object
-//        mTextPaint = new TextPaint();
-//        mTextPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
-//        mTextPaint.setTextAlign(Paint.Align.LEFT);
-//
-//        // Update TextPaint and text measurements from attributes
-//        invalidateTextPaintAndMeasurements();
-//    }
-//
-//    private void invalidateTextPaintAndMeasurements() {
-//        mTextPaint.setTextSize(mExampleDimension);
-//        mTextPaint.setColor(mExampleColor);
-//        mTextWidth = mTextPaint.measureText(mExampleString);
-//
-//        Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
-//        mTextHeight = fontMetrics.bottom;
-//    }
-//
-//    @Override
-//    protected void onDraw(Canvas canvas) {
-//        super.onDraw(canvas);
-//
-//        // TODO: consider storing these as member variables to reduce
-//        // allocations per draw cycle.
-//        int paddingLeft = getPaddingLeft();
-//        int paddingTop = getPaddingTop();
-//        int paddingRight = getPaddingRight();
-//        int paddingBottom = getPaddingBottom();
-//
-//        int contentWidth = getWidth() - paddingLeft - paddingRight;
-//        int contentHeight = getHeight() - paddingTop - paddingBottom;
-//
-//        // Draw the text.
-//        canvas.drawText(mExampleString,
-//                paddingLeft + (contentWidth - mTextWidth) / 2,
-//                paddingTop + (contentHeight + mTextHeight) / 2,
-//                mTextPaint);
-//
-//        // Draw the example drawable on top of the text.
-//        if (mExampleDrawable != null) {
-//            mExampleDrawable.setBounds(paddingLeft, paddingTop,
-//                    paddingLeft + contentWidth, paddingTop + contentHeight);
-//            mExampleDrawable.draw(canvas);
-//        }
-//    }
-//
-//    /**
-//     * Gets the example string attribute value.
-//     *
-//     * @return The example string attribute value.
-//     */
-//    public String getExampleString() {
-//        return mExampleString;
-//    }
-//
-//    /**
-//     * Sets the view's example string attribute value. In the example view, this string
-//     * is the text to draw.
-//     *
-//     * @param exampleString The example string attribute value to use.
-//     */
-//    public void setExampleString(String exampleString) {
-//        mExampleString = exampleString;
-//        invalidateTextPaintAndMeasurements();
-//    }
-//
-//    /**
-//     * Gets the example color attribute value.
-//     *
-//     * @return The example color attribute value.
-//     */
-//    public int getExampleColor() {
-//        return mExampleColor;
-//    }
-//
-//    /**
-//     * Sets the view's example color attribute value. In the example view, this color
-//     * is the font color.
-//     *
-//     * @param exampleColor The example color attribute value to use.
-//     */
-//    public void setExampleColor(int exampleColor) {
-//        mExampleColor = exampleColor;
-//        invalidateTextPaintAndMeasurements();
-//    }
-//
-//    /**
-//     * Gets the example dimension attribute value.
-//     *
-//     * @return The example dimension attribute value.
-//     */
-//    public float getExampleDimension() {
-//        return mExampleDimension;
-//    }
-//
-//    /**
-//     * Sets the view's example dimension attribute value. In the example view, this dimension
-//     * is the font size.
-//     *
-//     * @param exampleDimension The example dimension attribute value to use.
-//     */
-//    public void setExampleDimension(float exampleDimension) {
-//        mExampleDimension = exampleDimension;
-//        invalidateTextPaintAndMeasurements();
-//    }
-//
-//    /**
-//     * Gets the example drawable attribute value.
-//     *
-//     * @return The example drawable attribute value.
-//     */
-//    public Drawable getExampleDrawable() {
-//        return mExampleDrawable;
-//    }
-//
-//    /**
-//     * Sets the view's example drawable attribute value. In the example view, this drawable is
-//     * drawn above the text.
-//     *
-//     * @param exampleDrawable The example drawable attribute value to use.
-//     */
-//    public void setExampleDrawable(Drawable exampleDrawable) {
-//        mExampleDrawable = exampleDrawable;
-//    }
-//}
